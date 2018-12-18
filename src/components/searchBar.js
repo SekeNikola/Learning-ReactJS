e0cb934b6f314d1566ee47ef95066c3d4796127b
Seke Nikola <sekenikola@gmail.com>
2018-12-18 19:33:16 +0100

    fetch data

---
+++ src/components/searchBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    //! Uncontrol Element
+// onInputChange(event){
+//      console.log( event.target.value );
+// }
+
+//? Control Element
+
+    state = { term: ''}
+    onFormSubmit = (event) =>{
+        event.preventDefault()
+        this.props.onSubmit(this.state.term)
+    }
+    render() {
+        return (
+            //! Uncontrol Element
+            // <div className="ui segment">
+            //     <div className="field"></div>
+            //     <form className="ui form" action="">
+            //         <div className="field">
+            //             <label htmlFor="searcbar">Search for image</label>
+            //             <input type="text" onChange={this.onInputChange} />
+
+            //             {/* Alternate way to handler syntax */}
+            //             {/* <input type="text" onChange={(event) => console.log(event.target.value)}/> */}
+            //         </div>
+            //     </form>
+            // </div>
+
+
+            //? Control Element
+            <div className="ui segment">
+            <div className="field"></div>
+            {/* <form onSubmit={(event) => {event.preventDefault()}} className="ui form" action=""> */}
+            <form onSubmit={this.onFormSubmit} className="ui form" action="">
+                <div className="field">
+                    <label htmlFor="searchbar">Search for image</label>
+                    <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})} />
+                </div>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default SearchBar
