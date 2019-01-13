78a700fa4e52f7def7d5b229ce35512b7055d014
Seke Nikola <sekenikola@gmail.com>
2019-01-13 14:46:42 +0100

    add lodash and use memoize

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsopnPlaceHolder from '../apis/jsonplaceholder'
 
 export const fetchPost = () => async dispatch => {
@@ -9,11 +10,17 @@ export const fetchPost = () => async dispatch => {
     })
 };
 
-export const fetchUser = (id) => async dispatch => {
+
+// Using memoize action would be called one time and thats goiod for our application but if the post was updated we would not see the cnahges, We would have to cretae seperate functions to see that so memoize is not a good solution for this application
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsopnPlaceHolder.get(`/users/${id}`)
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     })
-}
+})
